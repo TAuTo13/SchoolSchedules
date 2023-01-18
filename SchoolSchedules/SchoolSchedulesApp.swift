023411acaedb3980812208ce9ef0717c5fad2e37
tadatota <knkyh13@gmail.com>
2023-01-18 11:56:59 +0900

    Initial Commit

---
+++ SchoolSchedules/SchoolSchedulesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolSchedulesApp.swift
+//  SchoolSchedules
+//
+//  Created by 多田桃大 on 2023/01/18.
+//
+
+import SwiftUI
+
+@main
+struct SchoolSchedulesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
